9c224c54a2fdf7910ead7578f45bd03288f96db3
JoseRinaldofilho <jose.vasconcelos@basis.com.br>
2021-10-11 17:24:58 -0300

    formularios

--- modulo-formularios-conicionais-sessao/script.php
+++ modulo-formularios-conicionais-sessao/script.php
@@ -1,1 +1,16 @@
 <?php
+// array de categorias
+$categoria = [];
+$categoria[] = 'infantil';
+$categoria[] = 'adolecente';
+$categoria[] = 'adulto';
+$categoria[] = 'idoso';
+//
+
+if (isset($_POST['acao'])){
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+}
+
+
